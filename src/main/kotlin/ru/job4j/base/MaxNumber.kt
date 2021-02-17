0aa20960f6b395a7d929f043fd25d8dd8ce02b39
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2021-02-17 22:39:26 +0300

    "Condition operators if" [#189221]

--- src/main/kotlin/ru/job4j/base/MaxNumber.kt
+++ src/main/kotlin/ru/job4j/base/MaxNumber.kt
@@ -1,8 +1,7 @@
 package ru.job4j.base
 
-fun max(first: Int, second: Int, third: Int) : Any {
-    return if (first > second && first > third) first
-    else if (second > first && second > third) second
-    else if (third > first && third > second) third
-    else "absent"
+fun maxOfTwo(first: Int, second: Int) = if (first > second) first else second
+
+fun maxOfThree(first: Int, second: Int, third: Int) : Int {
+    return maxOfTwo(maxOfTwo(first, second), third);
 }
--- src/test/kotlin/ru/job4j/base/MaxNumberTest.kt
+++ src/test/kotlin/ru/job4j/base/MaxNumberTest.kt
@@ -5,30 +5,30 @@ import io.kotlintest.specs.StringSpec
 
 class MaxNumberTest : StringSpec({
     "Max number between 29, 28, 17 is 29" {
-        max(29, 28, 17) shouldBe 29
+        maxOfThree(29, 28, 17) shouldBe 29
     }
 
     "Max number between 99, 100, 101 is 101" {
-        max(99, 100, 101) shouldBe 101
+        maxOfThree(99, 100, 101) shouldBe 101
     }
 
     "Max number between 45, 55, 50 is 55" {
-        max(45, 55, 50) shouldBe 55
+        maxOfThree(45, 55, 50) shouldBe 55
     }
 
     "Max number between 30, 30, 15 is absent" {
-        max(30, 30, 15) shouldBe "absent"
+        maxOfThree(30, 30, 15) shouldBe 30
     }
 
     "Max number between 14, 100, 100 is absent" {
-        max(14, 100, 100) shouldBe "absent"
+        maxOfThree(14, 100, 100) shouldBe 100
     }
 
     "Max number between -1, -2, -1 is absent" {
-        max(-1, -2, -1) shouldBe "absent"
+        maxOfThree(-1, -2, -1) shouldBe -1
     }
 
     "Max number between 7, 7, 7 is absent" {
-        max(7, 7, 7) shouldBe "absent"
+        maxOfThree(7, 7, 7) shouldBe 7
     }
 })
