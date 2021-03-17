baef713aecd7fdf7e77892dedc8f2eb2ebca4475
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2021-03-17 22:12:21 +0300

    "Class-companion" [#191433]

--- src/main/kotlin/ru/job4j/oop/tracker/StartUI.kt
+++ src/main/kotlin/ru/job4j/oop/tracker/StartUI.kt
@@ -2,77 +2,79 @@ package ru.job4j.oop.tracker
 
 /**
  * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
- * @version 1.0
+ * @version 1.1
  * @since 17.03.2021
  */
-object StartUI {
-
-    /**
-     * Метод инициализирует основные действия меню
-     */
-    fun init(tracker: Tracker) {
-        do {
-            getMenu()
-            val elect = readLine()!!
-            when (elect) {
-                "1" -> addItem(tracker)
-                "2" -> getAll(tracker)
-                else -> {
-                    if (elect != "3")
-                    println("---------------< Enter a correct number >---------------")
+class StartUI private constructor() {
+    companion object {
+        /**
+         * Метод инициализирует основные действия меню
+         */
+        fun init(tracker: Tracker) {
+            do {
+                getMenu()
+                val elect = readLine()!!
+                when (elect) {
+                    "1" -> addItem(tracker)
+                    "2" -> getAll(tracker)
+                    else -> {
+                        if (elect != "3")
+                            println("---------------< Enter a correct number >---------------")
+                    }
                 }
+            } while (exit(elect))
+        }
+
+
+        /**
+         * Метод выводит в консоль основное меню
+         */
+        private fun getMenu() {
+            println()
+            println("---------------< Enter a number >---------------")
+            println()
+            println("1. Add item")
+            println("2. Get all items")
+            println("3. Exit")
+        }
+
+        /**
+         * Метод дает возможность пользователю создать новую заявку
+         */
+        private fun addItem(tracker: Tracker) {
+            val item = Item()
+            println()
+            println("---------------< Enter item name >---------------")
+            val name = readLine()!!
+            item.name = name
+            println("---------------< Enter item description >---------------")
+            val description = readLine()!!
+            item.desc = description
+            val result = tracker.add(item)
+            println("---------------< New item with id " + result.id + " added! >---------------")
+        }
+
+        /**
+         * Метод дает возможность пользователю получить все созданные заявки
+         */
+        private fun getAll(tracker: Tracker) {
+            println()
+            println("---------------< All items of tracker >---------------")
+            tracker.findAll().forEach {
+                println(it)
             }
-        } while (exit(elect))
-    }
-
-    /**
-     * Метод выводит в консоль основное меню
-     */
-    private fun getMenu() {
-        println()
-        println("---------------< Enter a number >---------------")
-        println()
-        println("1. Add item")
-        println("2. Get all items")
-        println("3. Exit")
-    }
-
-    /**
-     * Метод дает возможность пользователю создать новую заявку
-     */
-    private fun addItem(tracker: Tracker) {
-        val item = Item()
-        println()
-        println("---------------< Enter item name >---------------")
-        val name = readLine()!!
-        item.name = name
-        println("---------------< Enter item description >---------------")
-        val description = readLine()!!
-        item.desc = description
-        val result = tracker.add(item)
-        println("---------------< New item with id " + result.id + " added! >---------------")
-    }
-
-    /**
-     * Метод дает возможность пользователю получить все созданные заявки
-     */
-    private fun getAll(tracker: Tracker) {
-        println()
-        println("---------------< All items of tracker >---------------")
-        tracker.findAll().forEach {
-            println(it)
         }
-    }
 
-    /**
-     * Метод дает возможность выйти из приложения
-     */
-    private fun exit(elect: String): Boolean {
-        if (elect == "3") {
-            println("---------------< See you soon >---------------")
-            return false
+        /**
+         * Метод дает возможность выйти из приложения
+         */
+        private fun exit(elect: String): Boolean {
+            if (elect == "3") {
+                println("---------------< See you soon >---------------")
+                return false
+            }
+            return true
         }
-        return true
     }
 }
 
