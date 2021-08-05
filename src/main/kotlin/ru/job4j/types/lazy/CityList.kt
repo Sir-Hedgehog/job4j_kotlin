982019d1477d58a2dfecdfa3eaebd0d8b2079a55
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2021-08-05 18:26:29 +0300

    "lazy" [#338938]

--- src/main/kotlin/ru/job4j/types/lazy/CityList.kt
+++ src/main/kotlin/ru/job4j/types/lazy/CityList.kt
@@ -1,8 +1,16 @@
 package ru.job4j.types.lazy
 
+/**
+ * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
+ * @version 1.0
+ * @since 05.08.2021
+ */
 class CityList {
     val ussrCities : List<String> by lazy { loadCities() }
 
+    /**
+     * Метод загружает список столиц СНГ
+     */
     private fun loadCities() : List<String> = listOf(
         "Moscow", "Minsk", "Kiev", "Yerevan", "Baku", "Tallinn", "Tbilisi", "Alma-Ata",
         "Frunze", "Riga", "Vilnius", "Kishinev", "Dushanbe", "Ashkhabad", "Tashkent"
